50a4d827b68cfc3345a6e10cb6d6caf5057d25eb
Paul Camerloher <paul.camerloher.qsc@gmail.com>
2020-11-11 15:30:25 +0100

    Update

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -11,6 +11,7 @@ namespace GitApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Test123");
+            Console.WriteLine("Test123Test");
         }
     }
 }
